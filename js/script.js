cb7618d075b2b2e9a56b22e421b9bc96e60c4a59
Jbblokker <76689818+Jbblokker@users.noreply.github.com>
2021-01-13 20:36:36 -0600

    added a string array
    added an array with objects to begin process of app creation.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+//this is an arry of pokemon. we are using name, height, and type as objects.
+let pokemonList = [
+  { name: 'Blastoise', height: 5, type: 'water' },
+  { name: 'Charzard', height: 5, type: [ 'fire', 'flying'] },
+  { name: 'Lapras', height: 8, type: [ 'water','ice'] }
+];
