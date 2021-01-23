5bed0653e61f7d44bf12d0077247fd2c600b6436
Jbblokker <76689818+Jbblokker@users.noreply.github.com>
2021-01-23 08:07:34 -0600

    removed for loop to test usting forEach
    made the for loop a comment to be able to access at a later date if needed. By removing this I am adding the forEach to pull information from the array from within the iife

--- js/script.js
+++ js/script.js
@@ -15,12 +15,17 @@ let pokemonList = [
   type: [ 'water','ice']
   }
 ];
-//
-for (let i=0; i < pokemonList.length; i++){
-  if (pokemonList[i].height <6 && pokemonList[i].height >4){
-    document.write('<br>' + pokemonList[i].name + ' height is ' + pokemonList[i].height );
-  }else if (pokemonList[i].height >7){
-    document.write('<br>' + pokemonList[i].name + ' height is ' + pokemonList[i].height
-    + ', Wow that is one big Pokemon');
-  }
-}
+//forEach value being used to display array.
+pokemonRepository.getAll().forEach(function(pokemon) {
+  document.write('<br>' + 'My name is ' + pokemon.name + ' and I am ' + ' (height: ' +
+pokemon.height + ')' + ' tall, and I am a ' + pokemon.type + ' pokemon.' );
+});
+
+pokemonRepository.add({name: 'Bulbasaur '});
+//For loop that was previously used to display pokemonList
+//for (let i=0; i < pokemonList.length; i++){
+  //if (pokemonList[i].height <= 7){
+    //document.write('<br>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')' );
+  //}else{
+    //document.write('<br>' + pokemonList[i].name + ' (height: ' + pokemonList[i].height + ')'
+    //+ ',Wow that is one big Pokemon');
