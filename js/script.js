b5df31de03075ff1cca688edbbe0338feb9e3081
Jbblokker <76689818+Jbblokker@users.noreply.github.com>
2021-01-30 11:38:29 -0600

    created a new functions  to fetch data from apiUrl

--- js/script.js
+++ js/script.js
@@ -10,25 +10,60 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
       return pokemonList;
     }
 
-  function showDetails(pokemon){
-    console.log(pokemon.name, pokemon.height);
+  function showDetails(pokemon) {
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
+  function loadList() {
+    return fetch (apiUrl).then (function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name:item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+        console.log(pokemon)
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+      let url = item.detailsUrl;
+      return fetch(url).then(function (response) {
+        return response.json();
+      }).then(function (details) {
+        // Now we add the details to the item
+        item.imageUrl = details.sprites.front_default;
+        item.height = details.height;
+        item.types = details.types;
+      }).catch(function (e) {
+        console.error(e);
+      });
+    }
 
   function addListItem(pokemon){
     let pokemonList = document.querySelector('.pokemon-list');
     let listpokemon = document.createElement('li');
     let button = document.createElement('button');
-    button.addEventListener('click', function () {
-      showDetails(pokemon)
-    });
     button.innerText = pokemon.name;
     button.classList.add('button-class');
     listpokemon.appendChild(button);
     pokemonList.appendChild(listpokemon);
+    button.addEventListener('click', function (event) {
+      showDetails(pokemon)
+    });
   }
   return { add: add,
      getAll: getAll,
-     addListItem: addListItem
+     addListItem: addListItem,
+     loadList: loadList,
+     loadDetails: loadDetails,
+     showDetails: showDetails
   };
 })();
 //forEach value being used to display array.
