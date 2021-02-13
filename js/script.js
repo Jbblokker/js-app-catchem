8cfbb0c5b0cc07e1783cec9967b76c02dacb062d
Jbblokker <jbblokker@outlook.com>
2021-02-13 07:04:44 -0600

    added a search function

--- js/script.js
+++ js/script.js
@@ -82,7 +82,27 @@ pokemonRepository.loadList().then(function() {
   });
 });
 
+function search() {
+  // Declare variables
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('myInput');
+  filter = input.value.toUpperCase();
+  ul = document.getElementById("myUL");
+  li = ul.querySelectorAll(".card");
+  //li = ul.getElementsByTagName('li');
 
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    a = li[i].querySelector(".card-body").querySelector(".card.title");
+    console.log(a.innerText);
+    txtValue = a.textContent
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = "";
+    } else {
+      li[i].style.display = "none";
+    }
+  }
+}
 
 
 //document.write used to display pokemon from array
