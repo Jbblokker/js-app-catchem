53fc353145b17a32669a4b285e54a5db9482b430
Jbblokker <jbblokker@outlook.com>
2021-02-13 07:06:09 -0600

    new modal information added
    new modal information added show through new bootstrap modal

--- js/script.js
+++ js/script.js
@@ -49,9 +49,67 @@ let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
       });
     }
 
+    function addListItem(pokemon) {
+     pokemonRepository.loadDetails(pokemon).then(function () {
+       let row = $(".row");
+
+       let card = $(
+         '<div class="card mt-5" style="width: 18rem; margin:13px;"></div>'
+       );
+       let image = $(
+         '<img class="card-img-top mx-auto" style="width:30%;" alt="...">'
+       );
+       let title = $('<h5 class="card-title">' + pokemon.name + "</h5>");
+       image.attr("src", pokemon.imageUrl);
+       let body = $('<div class="card-body" style="text-align: center;"></div>');
+       let button = $(
+         '<button type="button" class="btn" style="background-color: #f0e446; color: black" data-toggle="modal" data-target="#exampleModal">Profile</button>'
+       );
+
+       // Append
+       row.append(card);
+       card.append(image);
+       card.append(body);
+       body.append(title);
+       body.append(button);
+    button.on('click', function (event) {
       showDetails(pokemon);
     });
   });
+}
+let modalConatiner = $("#modal-container");
+//show Modal function
+function showModal(item) {
+ let modalBody = $('.modal-body');
+ let modalTitle = $('modal-title');
+ let modalHeader = $('modal-header');
+ modalTitle.empty();
+ modalBody.empty();
+
+ //creating element ofr name in modal contentElement
+ let nameElement = $('<h1>' + item.name + '</h1>');
+ // creating an inmage in modal content
+//// let imageElement = $('<img class='modal-img' style='width50%'>');
+  imageElement.atrr('src', item.imageUrl);
+ //creating element for height in modal content
+ let heightElement = $('<p>' + 'height : ' + item.height + '</p>');
+ //creating element for weight in modal content
+ let weightElement = $('<p>' + 'weight : ' + item.weight + '</p>');
+ //creating element for type in the modal contentElement
+ let typesElement = $('<p>' + 'type : ' + item.types + '</p>');
+ //creating element for the abilities in modal content
+ let abilitiesElement = $('<p>' + 'abilities : ' + item.abilities + '</p>');
+
+ modalTitle.append(nameElement);
+// modalBody.append(imageElement);
+ modalBody.append(heightElement);
+ modalBody.append(weightElement);
+ modalBody.append(typesElement);
+ modalBody.append(abilitiesElement);
+
+modalContainer.addClass("is-visible");
+
+}
 
 
   return { add: add,
